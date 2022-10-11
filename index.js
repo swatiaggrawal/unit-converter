d70c30b6ee8b9f36920597e6ee4c1bb8de17731d
Swati Aggrawal <swatiagg357@gmail.com>
2022-10-11 20:42:15 +0530

    updates

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 const renderLength=document.getElementById("render-len")
 const renderVolume=document.getElementById("render-vol")
 const renderMass=document.getElementById("render-mass")
+let lenMeter, lenFeet, volLiter, volGallon, massGrams, masspounds;
 
+
+const inputEl=document.getElementById("input-measurement")
+const inputBtn=document.getElementById("input-btn")
+inputBtn.addEventListener("click",function (){
+    
+    lenMeter=inputEl.value/3.28084
+    lenFeet=inputEl.value*3.28084
+    volLiter=inputEl.value*3.785411784
+    volGallon=inputEl.value/3.785411784
+    masspounds=inputEl.value*2.20462262185 
+    massGrams=inputEl.value/2.20462262185 
+    render(lenMeter, lenFeet, volLiter, volGallon, massGrams, masspounds)
+})
+
+function render(lenMeter, lenFeet, volLiter, volGallon, massGrams, masspounds){
+    renderLength.innerHTML=`${inputEl.value} meter = ${lenFeet.toFixed(3)} feet | ${inputEl.value} feet = ${lenMeter.toFixed(3)} meter`
+    renderVolume.innerHTML=`${inputEl.value} liter = ${volGallon.toFixed(3)} gallon | ${inputEl.value} gallon = ${volLiter.toFixed(3)} liter`
+    renderMass.innerHTML=`${inputEl.value} kilo = ${masspounds.toFixed(3)} pound | ${inputEl.value} pound = ${massGrams.toFixed(3)} kilo`
+    
+
+}
